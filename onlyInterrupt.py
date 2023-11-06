560d8d621866cba30c623fe9bfa460bebfb64c11
Shawn Rast <rastshawn@gmail.com>
2023-11-06 05:28:12 +0000

    basic reading of throttle position sensor via ads1115

--- onlyInterrupt.py
+++ onlyInterrupt.py
@@ -3,7 +3,7 @@ import RPi.GPIO as GPIO
 import time
 import threading
 import os
-
+import Adafruit_ADS1x15 # for TPS ADC
 
 
 ###### This section manages reading the tach signals and assigns them to g_rpm
@@ -18,6 +18,14 @@ fps = 3 #must be at least 2
 dt = datetime.now()
 
 
+###### Set up TPS ADC
+# Default gain is 1, for +/-4.096V. Because the ADS1115 is powered at 3.3v,
+# max actual input is 3.6v
+GAIN = 1
+adc = Adafruit_ADS1x15.ADS1115()
+TPS_0_PERCENT = 800
+TPS_100_PERCENT = 5650
+TPS_RANGE = TPS_100_PERCENT - TPS_0_PERCENT
 
 # every time there's a pulse, this calculates the rpm and sets g_rpm
 def pulseInterrupt(channel):
@@ -54,9 +62,19 @@ def simulateRPMchange():
 print('time, rpm')
 startTime = datetime.now()
 def printRow():
-    print(f'{(datetime.now()-startTime).total_seconds()}, {g_rpm}')
-    threading.Timer(0.05, printRow).start()
+    
+    runningSeconds = (datetime.now()-startTime).total_seconds()
+    tpsRawValue = adc.read_adc(0, gain=GAIN)
+    if tpsRawValue < TPS_0_PERCENT:
+        tpsRawValue = TPS_0_PERCENT
+    elif tpsRawValue > TPS_100_PERCENT:
+        tpsRawValue = TPS_100_PERCENT
+    tpsRangedValue = tpsRawValue - TPS_0_PERCENT
 
+    tpsPercent = 100 * (tpsRangedValue / TPS_RANGE)  # todo. Maybe autoset max and min?
+    print(f'{(datetime.now()-startTime).total_seconds()}, {g_rpm}, {tpsPercent}')
+    threading.Timer(0.05, printRow).start()
+    
 printRow()
 
 
